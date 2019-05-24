fc52712b9b521fc2a347fb62701223a143567ce7
PetrenkoRoman <raser.group@gmail.com>
2019-05-24 13:37:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,61 @@
-$('#btn').click(function () {
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "https://api.novaposhta.ua/v2.0/json/",
-            data: JSON.stringify({
-                "modelName": "TrackingDocument",
-                "calledMethod": "getStatusDocuments",
-                "methodProperties": {
-                    "Documents": [
-					
-					
-		{
-            "DocumentNumber": "20450118862059",
-            "Phone":""
-        },
-        {
-            "DocumentNumber": "20450118862056",
-            "Phone":""
-        }
-					
-
-					]
-                },
-                "apiKey": "caeb13b660b11432a9843510578b909a"
-            }),
-            success: function(response) {
-                var data = response.data;
-                data.forEach(function (item, i, arr) {
-                   $('.status').append(item.Number);
-                   $('.status').append('<br>');
-                   $('.status').append(item.Status);
-                   $('.status').append('<br><br>');
+$('.status-btn').on('click', function (event) {
+	var id = $(this).parent().children('div').text()
+	$.ajax({
+	    type: "POST",
+	    dataType: "json",
+	    url: "https://api.novaposhta.ua/v2.0/json/",
+	    data: JSON.stringify({
+	        "modelName": "TrackingDocument",
+	        "calledMethod": "getStatusDocuments",
+	        "methodProperties": {
+	            "Documents": [
+	                {
+	                    "DocumentNumber": id,
+	                    "Phone": ""
+	                }]
+	        },
+	        "apiKey": "caeb13b660b11432a9843510578b909a"
+	    }),
+	    success: function(response) {
+	        var data = response.data;
+	        data.forEach(function(item, i, arr) {
+	            $(event.target.parentNode).append(item.Status);
+	        });
+	    },
+	})
     
-                });
-				
-            },
-        }); 
-        });
+})
+
+$('.allstatus-btn').on('click', function (event) {
+  
+$.each($(' .number-all'), function (index, value) { 
+  	var id = $(value).text(),
+        $this = $(this);
+    
+  $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "https://api.novaposhta.ua/v2.0/json/",
+      data: JSON.stringify({
+          "modelName": "TrackingDocument",
+          "calledMethod": "getStatusDocuments",
+          "methodProperties": {
+              "Documents": [
+                  {
+                      "DocumentNumber": id,
+                      "Phone": ""
+                  }]
+          },
+          "apiKey": "caeb13b660b11432a9843510578b909a"
+      }),
+      success: function(response) {
+          var data = response.data;
+          data.forEach(function(item, i, arr) {
+              $this.append(item.Status);
+          });
+      },
+  })
+  console.log($(value).text()); 
+});
+
+})
